9d40180a94309f68372e54de068e8a2f0eb0e16c
Shiien <935697492@qq.com>
2021-09-14 20:33:02 +0800

    add torchbeast

--- simple_train.py
+++ simple_train.py
@@ -1,21 +1,230 @@
 import utils.atari_wrapper as atari_wrapper
-from utils.multi import create_buffers
+from utils.multi import create_buffers, get_batch, act
+import torch.multiprocessing as mp
+# from utils
+import argparse
+from model.atari_impala import AtariCasaNet
+import logging
+import torch
+import time
+import timeit
+import os
+import utils.some_trace as some_trace
+import utils.some_loss as some_loss
+import threading
+import pprint
+from utils import file_writer,prof
+os.environ["OMP_NUM_THREADS"] = "1"  # Necessary for multithreading.
 
 
-def create_env():
+def create_env(name):
     return atari_wrapper.wrap_pytorch(
         atari_wrapper.wrap_deepmind(
-            atari_wrapper.make_atari_by_id('PongNoFrameskip-v0'),
+            atari_wrapper.make_atari_by_id(name),  # PongNoFrameskip-v0
             clip_rewards=False,
             frame_stack=True,
             scale=False
-        ))
+        )
+    )
 
 
-if __name__ == '__main__':
-    env = create_env()
-    print(env.action_space.n)
-    buffers = create_buffers(30, 5, env.observation_space.shape, env.action_space.n)
+parser = argparse.ArgumentParser(description="PyTorch Scalable Agent")
+
+parser.add_argument("--env", type=str, default="PongNoFrameskip-v4",
+                    help="Gym environment.")
+parser.add_argument("--mode", default="train",
+                    choices=["train", "test", "test_render"],
+                    help="Training or test mode.")
+parser.add_argument("--xpid", default=None,
+                    help="Experiment id (default: None).")
+
+# Training settings.
+parser.add_argument("--disable_checkpoint", action="store_true",
+                    help="Disable saving checkpoint.")
+parser.add_argument("--savedir", default="~/logs/torchbeast",
+                    help="Root dir where experiment data will be saved.")
+parser.add_argument("--num_actors", default=16, type=int, metavar="N",
+                    help="Number of actors (default: 4).")
+parser.add_argument("--total_steps", default=2 * int(1e6), type=int, metavar="T",
+                    help="Total environment steps to train for.")
+parser.add_argument("--batch_size", default=4, type=int, metavar="B",
+                    help="Learner batch size.")
+parser.add_argument("--unroll_length", default=80, type=int, metavar="T",
+                    help="The unroll length (time dimension).")
+parser.add_argument("--num_buffers", default=None, type=int,
+                    metavar="N", help="Number of shared-memory buffers.")
+parser.add_argument("--num_learner_threads", "--num_threads", default=4, type=int,
+                    metavar="N", help="Number learner threads.")
+parser.add_argument("--disable_cuda", action="store_true",
+                    help="Disable CUDA.")
+parser.add_argument("--use_lstm", action="store_true",
+                    help="Use LSTM in agent model.")
+
+# Loss settings.
+parser.add_argument("--entropy_cost", default=0.0006,
+                    type=float, help="Entropy cost/multiplier.")
+parser.add_argument("--q_cost", default=1,
+                    type=float, help="q cost/multiplier.")
+parser.add_argument("--baseline_cost", default=0.5,
+                    type=float, help="Baseline cost/multiplier.")
+parser.add_argument("--discounting", default=0.99,
+                    type=float, help="Discounting factor.")
+parser.add_argument("--reward_clipping", default="abs_one",
+                    choices=["abs_one", "none"],
+                    help="Reward clipping.")
+
+# Optimizer settings.
+parser.add_argument("--learning_rate", default=0.00048,
+                    type=float, metavar="LR", help="Learning rate.")
+parser.add_argument("--alpha", default=0.99, type=float,
+                    help="RMSProp smoothing constant.")
+parser.add_argument("--momentum", default=0, type=float,
+                    help="RMSProp momentum.")
+parser.add_argument("--epsilon", default=0.01, type=float,
+                    help="RMSProp epsilon.")
+parser.add_argument("--grad_norm_clipping", default=40.0, type=float,
+                    help="Global gradient norm clip.")
+logging.basicConfig(
+    format=(
+        "[%(levelname)s:%(process)d %(module)s:%(lineno)d %(asctime)s] " "%(message)s"
+    ),
+    level=0,
+)
+
+
+def casalearn(
+        flags,
+        actor_model,
+        model,
+        batch,
+        initial_agent_state,
+        optimizer,
+        scheduler,
+        lock=threading.Lock(),  # noqa: B008
+):
+    """Performs a learning (optimization) step."""
+    with lock:
+        # print('learn start')
+        learner_outputs, unused_state = model(batch, initial_agent_state)
+
+        # print('forward ok')
+        # Take final value function slice for bootstrapping.
+        bootstrap_value = learner_outputs["baseline"][-1]
+        bootstrap_qvalue = learner_outputs["q"][-1]
+        # Move from obs[t] -> action[t] to action[t] -> obs[t].
+        batch = {key: tensor[1:] for key, tensor in batch.items()}
+        learner_outputs = {key: tensor[:-1] for key, tensor in learner_outputs.items()}
+
+        rewards = batch["reward"]
+        if flags.reward_clipping == "abs_one":
+            clipped_rewards = torch.clamp(rewards, -1, 1)
+        elif flags.reward_clipping == "none":
+            clipped_rewards = rewards
+
+        discounts = (~batch["done"]).float() * flags.discounting
+
+        vtrace_returns = some_trace.v_trace_from_logits(
+            behavior_policy_logits=batch["policy_logits"],
+            target_policy_logits=learner_outputs["policy_logits"],
+            actions=batch["action"],
+            discounts=discounts,
+            rewards=clipped_rewards,
+            values=learner_outputs["baseline"],
+            bootstrap_value=bootstrap_value,
+        )
+        # print('vtrace ok')
+        retrace_returns = some_trace.retrace_from_logits(
+            behavior_policy_logits=batch["policy_logits"],
+            target_policy_logits=learner_outputs["policy_logits"],
+            actions=batch["action"],
+            discounts=discounts,
+            rewards=clipped_rewards,
+            q_values=learner_outputs["q"],
+            bootstrap_value=bootstrap_qvalue,
+        )
+        # print('retrace ok')
+        pg_loss = some_loss.compute_policy_gradient_loss(
+            learner_outputs["policy_logits"],
+            batch["action"],
+            vtrace_returns.pg_advantages,
+        )
+        baseline_loss = flags.baseline_cost * some_loss.compute_baseline_loss(
+            vtrace_returns.vs - learner_outputs["baseline"]
+        )
+        entropy_loss = flags.entropy_cost * some_loss.compute_entropy_loss(
+            learner_outputs["policy_logits"]
+        )
+
+        q_loss = flags.q_cost * some_loss.compute_baseline_loss(
+            retrace_returns.qs - learner_outputs["q_action"]
+        )
+
+        total_loss = pg_loss + baseline_loss + entropy_loss + q_loss
+
+        episode_returns = batch["episode_return"][batch["done"]]
+
+        optimizer.zero_grad()
+        total_loss.backward()
+        grad = torch.nn.utils.clip_grad_norm_(model.parameters(), flags.grad_norm_clipping)
+        optimizer.step()
+        scheduler.step()
+        # print('update ok')
+        stats = {
+            "episode_returns": tuple(episode_returns.cpu().numpy()),
+            "mean_episode_return": torch.mean(episode_returns).item(),
+            "total_loss": total_loss.item(),
+            "pg_loss": pg_loss.item(),
+            "baseline_loss": baseline_loss.item(),
+            "entropy_loss": entropy_loss.item(),
+            "q_loss": q_loss.item(),
+            "grad_norm": grad.norm().item()
+        }
+        actor_model.load_state_dict(model.state_dict())
+        return stats
+
+
+Net = AtariCasaNet
+
+
+def main(flags):
+    initial_agent_state_buffers = []
+
+    if flags.xpid is None:
+        flags.xpid = "torchbeast-%s" % time.strftime("%Y%m%d-%H%M%S")
+    plogger = file_writer.FileWriter(
+        xpid=flags.xpid, xp_args=flags.__dict__, rootdir=flags.savedir
+    )
+    checkpointpath = os.path.expandvars(
+        os.path.expanduser("%s/%s/%s" % (flags.savedir, flags.xpid, "model.tar"))
+    )
+
+    if flags.num_buffers is None:  # Set sensible default for num_buffers.
+        flags.num_buffers = max(2 * flags.num_actors, flags.batch_size)
+    if flags.num_actors >= flags.num_buffers:
+        raise ValueError("num_buffers should be larger than num_actors")
+    if flags.num_buffers < flags.batch_size:
+        raise ValueError("num_buffers should be larger than batch_size")
+
+    T = flags.unroll_length
+    B = flags.batch_size
+
+    flags.device = None
+    if not flags.disable_cuda and torch.cuda.is_available():
+        logging.info("Using CUDA.")
+        flags.device = torch.device("cuda:1")
+    else:
+        logging.info("Not using CUDA.")
+        flags.device = torch.device("cpu")
+
+    env = create_env(flags.env)
+
+    model = Net(env.observation_space.shape, env.action_space.n, flags.use_lstm)
+    logging.info('start create_buffers')
+
+    buffers = create_buffers(flags.unroll_length, flags.num_buffers, env.observation_space.shape, model.num_actions)
+
+    model.share_memory()
+    # Add initial RNN state.
     initial_agent_state_buffers = []
     for _ in range(flags.num_buffers):
         state = model.initial_state(batch_size=1)
@@ -39,6 +248,7 @@ if __name__ == '__main__':
                 model,
                 buffers,
                 initial_agent_state_buffers,
+                logging
             ),
         )
         actor.start()
@@ -56,10 +266,8 @@ if __name__ == '__main__':
         alpha=flags.alpha,
     )
 
-
     def lr_lambda(epoch):
         return 1 - min(epoch * T * B, flags.total_steps) / flags.total_steps
-
 
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
 
@@ -75,7 +283,6 @@ if __name__ == '__main__':
 
     step, stats = 0, {}
 
-
     def batch_and_learn(i, lock=threading.Lock()):
         """Thread target for the learning process."""
         nonlocal step, stats
@@ -88,9 +295,10 @@ if __name__ == '__main__':
                 full_queue,
                 buffers,
                 initial_agent_state_buffers,
-                timings,
+                timings=timings
             )
-            stats = learn(
+            # print('get batch ok')
+            stats = casalearn(
                 flags, model, learner_model, batch, agent_state, optimizer, scheduler
             )
             timings.time("learn")
@@ -100,9 +308,8 @@ if __name__ == '__main__':
                 plogger.log(to_log)
                 step += T * B
 
-        if i == 0:
-            logging.info("Batch and learn: %s", timings.summary())
-
+            if i == 0:
+                logging.info("Batch and learn %s",timings.summary())
 
     for m in range(flags.num_buffers):
         free_queue.put(m)
@@ -114,7 +321,6 @@ if __name__ == '__main__':
         )
         thread.start()
         threads.append(thread)
-
 
     def checkpoint():
         if flags.disable_checkpoint:
@@ -130,14 +336,13 @@ if __name__ == '__main__':
             checkpointpath,
         )
 
-
     timer = timeit.default_timer
     try:
         last_checkpoint_time = timer()
         while step < flags.total_steps:
             start_step = step
             start_time = timer()
-            time.sleep(5)
+            time.sleep(20)
 
             if timer() - last_checkpoint_time > 10 * 60:  # Save every 10 min.
                 checkpoint()
@@ -152,7 +357,7 @@ if __name__ == '__main__':
                 mean_return = ""
             total_loss = stats.get("total_loss", float("inf"))
             logging.info(
-                "Steps %i @ %.1f SPS. Loss %f. %sStats:\n%s",
+                "Steps %i @ %.1f SPS. Loss %f. %s \nStats: %s",
                 step,
                 sps,
                 total_loss,
@@ -173,4 +378,8 @@ if __name__ == '__main__':
 
     checkpoint()
     plogger.close()
-    # print(env.reset())
+
+
+if __name__ == "__main__":
+    flags = parser.parse_args()
+    main(flags)
--- utils/multi.py
+++ utils/multi.py
@@ -1,12 +1,29 @@
+import logging
+import time
 import typing
 import torch
 import torch.multiprocessing as mp
 import threading
+import os
+from utils.environment import Environment
+import utils.atari_wrapper as atari_wrappers
+from utils import prof
+
 Buffers = typing.Dict[str, typing.List[torch.Tensor]]
 
 
+def create_env(flags):
+    return atari_wrappers.wrap_pytorch(
+        atari_wrappers.wrap_deepmind(
+            atari_wrappers.make_atari(flags.env),
+            clip_rewards=False,
+            frame_stack=True,
+            scale=False,
+        )
+    )
 
-def create_buffers(unroll_length,num_buffers, obs_shape, num_actions) -> Buffers:
+
+def create_buffers(unroll_length, num_buffers, obs_shape, num_actions) -> Buffers:
     T = unroll_length
     specs = dict(
         frame=dict(size=(T + 1, *obs_shape), dtype=torch.uint8),
@@ -17,6 +34,8 @@ def create_buffers(unroll_length,num_buffers, obs_shape, num_actions) -> Buffers
         policy_logits=dict(size=(T + 1, num_actions), dtype=torch.float32),
         baseline=dict(size=(T + 1,), dtype=torch.float32),
         last_action=dict(size=(T + 1,), dtype=torch.int64),
+        q=dict(size=(T + 1, num_actions), dtype=torch.float32),
+        q_action=dict(size=(T + 1,), dtype=torch.float32),
         action=dict(size=(T + 1,), dtype=torch.int64),
     )
     buffers: Buffers = {key: [] for key in specs}
@@ -24,14 +43,16 @@ def create_buffers(unroll_length,num_buffers, obs_shape, num_actions) -> Buffers
         for key in buffers:
             buffers[key].append(torch.empty(**specs[key]).share_memory_())
     return buffers
+
+
 def get_batch(
-    flags,
-    free_queue: mp.SimpleQueue,
-    full_queue: mp.SimpleQueue,
-    buffers: Buffers,
-    initial_agent_state_buffers,
-    timings,
-    lock=threading.Lock(),
+        flags,
+        free_queue: mp.SimpleQueue,
+        full_queue: mp.SimpleQueue,
+        buffers: Buffers,
+        initial_agent_state_buffers,
+        timings,
+        lock=threading.Lock(),
 ):
     with lock:
         timings.time("lock")
@@ -54,3 +75,68 @@ def get_batch(
     )
     timings.time("device")
     return batch, initial_agent_state
+
+
+def act(
+        flags,
+        actor_index: int,
+        free_queue: mp.SimpleQueue,
+        full_queue: mp.SimpleQueue,
+        model: torch.nn.Module,
+        buffers: Buffers,
+        initial_agent_state_buffers,
+        logging
+):
+    try:
+        logging.info('Actor %i start', actor_index)
+        timings = prof.Timings()
+        gym_env = create_env(flags)
+        seed = actor_index ^ int.from_bytes(os.urandom(4), byteorder="little")
+        gym_env.seed(seed)
+        env = Environment(gym_env)
+        env_output = env.initial()
+        agent_state = model.initial_state(batch_size=1)
+        agent_output, unused_state = model(env_output, agent_state)
+        while True:
+            index = free_queue.get()
+            if index is None:
+                break
+
+            # Write old rollout end.
+            # print(actor_index,'write old rollout end env')
+            for key in env_output:
+                buffers[key][index][0, ...] = env_output[key]
+
+            # print(actor_index,'write old rollout end agent')
+            for key in agent_output:
+                buffers[key][index][0, ...] = agent_output[key]
+            for i, tensor in enumerate(agent_state):
+                initial_agent_state_buffers[index][i][...] = tensor
+
+            for t in range(flags.unroll_length):
+                timings.reset()
+                with torch.no_grad():
+                    agent_output, agent_state = model(env_output, agent_state)
+                timings.time("model")
+                env_output = env.step(agent_output["action"])
+
+                timings.time("step")
+                for key in env_output:
+                    buffers[key][index][t + 1, ...] = env_output[key]
+                # print('write env')
+                for key in agent_output:
+                    buffers[key][index][t + 1, ...] = agent_output[key]
+                # print('write agent')
+
+                timings.time("write")
+            full_queue.put(index)
+            if actor_index == 0:
+                logging.info("Actor %i: %s", actor_index, timings.summary())
+            # logging.info(,actor_index)
+
+
+    except KeyboardInterrupt:
+        pass  # Return silently.
+    except Exception as e:
+        print()
+        raise e
